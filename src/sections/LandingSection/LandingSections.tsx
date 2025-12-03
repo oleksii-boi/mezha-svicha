29d688504521528ccd7e0d7cd969e2406dfb011d
oboiko <oboiko@paylocity.com>
2025-12-03 15:16:01 +0100

    add offer file

--- src/sections/LandingSection/LandingSections.tsx
+++ src/sections/LandingSection/LandingSections.tsx
@@ -1,7 +1,17 @@
 import LogoFull from '../../assets/logo-full.svg';
+import PdfFile from '../../assets/Межа Свіча Пропозиція.pdf';
 import styles from './LandingSections.module.scss';
 
 export const LandingSections = () => {
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = PdfFile;
+    link.download = 'Межа Свіча Пропозиція.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <section className={styles.sectionContainer}>
       <img
@@ -10,7 +20,9 @@ export const LandingSections = () => {
         alt="Межа Свіча - логотип локального фермерського господарства"
       />
 
-      <button className={styles.button}>ЗАВАНТАЖИТИ ПРОПОЗИЦІЮ</button>
+      <button className={styles.button} onClick={handleDownload}>
+        ЗАВАНТАЖИТИ ПРОПОЗИЦІЮ
+      </button>
     </section>
   );
 };
